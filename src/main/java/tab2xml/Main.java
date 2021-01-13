a1bcb486f5e0d36cf6861716a6eab5f5e559aa06
amir <amir@192.168.0.130>
2021-01-13 13:18:22 -0500

    Amir's first commit 😀

--- src/main/java/tab2xml/Main.java
+++ src/main/java/tab2xml/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Goodbye, World!");
+		System.out.println("Amir's commit");
 	}
 	
 }
