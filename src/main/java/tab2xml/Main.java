2d21e14e29b8d4b3548677b2242333b6ce1bce4a
Adrien Hopkins <ahopk127@my.yorku.ca>
2021-01-13 13:33:03 -0500

    merge test

--- src/main/java/tab2xml/Main.java
+++ src/main/java/tab2xml/Main.java
@@ -6,12 +6,8 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, World!");
 		System.out.println("hey, World!");
-<<<<<<< HEAD
 		System.out.println("edward's commit");
-=======
-		
 		// ahopk127
->>>>>>> branch 'master' of https://github.com/ahopk127/eecs2311-tab2xml
 	}
 	
 }
