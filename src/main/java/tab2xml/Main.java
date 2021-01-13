f7159881dfd9f53a0c0575036ff8e77b4179c91f
amir <amir@192.168.0.130>
2021-01-13 13:48:54 -0500

    Added commit to main.

--- src/main/java/tab2xml/Main.java
+++ src/main/java/tab2xml/Main.java
@@ -3,12 +3,11 @@ package tab2xml;
 public class Main {
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello, World!");
 		System.out.println("hey, World!");
 		System.out.println("edward's commit");
 		System.out.println("Sayed's commit");
-		// ahopk127
+		System.out.println("Amir's commit");
 	}
 	
 }
