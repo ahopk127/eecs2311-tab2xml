2a4b7de75cc005b4a71f9eec20a8f7d6b185ada3
Sayed Mohammed <sayed08@my.yorku.ca>
2021-01-20 11:49:17 -0500

    Sayed's commit

--- src/main/java/tab2xml/Main.java
+++ src/main/java/tab2xml/Main.java
@@ -4,10 +4,7 @@ public class Main {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
-		System.out.println("hey, World!");
-		System.out.println("edward's commit");
-		System.out.println("Sayed's commit");
-		System.out.println("Amir's commit");
+		System.out.println("Sayed, World!");
 	}
 	
 }
