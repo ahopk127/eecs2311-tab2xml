0b3ce978c5a81c52adc95839986f819470b0bff0
Sayed Mohammed <sayed08@my.yorku.ca>
2021-01-25 13:06:20 -0500

    sayed's commmit

--- src/main/java/tab2xml/Main.java
+++ src/main/java/tab2xml/Main.java
@@ -3,11 +3,11 @@ package tab2xml;
 public class Main {
 	
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
 		System.out.println("Sayed, World!");
-=======
+
 		System.out.println("Adrien");
->>>>>>> branch 'master' of https://github.com/ahopk127/eecs2311-tab2xml.git
+
 		System.out.println("Sayed, World!");
 		// edwards commit
 		// amir's commit
