a5cb2c86f144622dd8abd35adfac2f396922b5bc
amir <amirabdalla23@gmail.com>
2021-01-27 12:04:06 -0500

    Lab task test.

--- src/main/java/tab2xml/Main.java
+++ src/main/java/tab2xml/Main.java
@@ -3,11 +3,9 @@ package tab2xml;
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
-
+		System.out.println("amir");
 		//edward
 		//edward 2
-
 	}
 	
 }
