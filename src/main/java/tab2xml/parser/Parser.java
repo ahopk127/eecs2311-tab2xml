f89aca55fe4d92335412d7861e0ecb302156e86e
amir <amirabdalla23@gmail.com>
2021-01-27 14:28:21 -0500

    small format changes.

--- src/main/java/tab2xml/parser/Parser.java
+++ src/main/java/tab2xml/parser/Parser.java
@@ -13,12 +13,12 @@ public class Parser {
 	/*
 	 * Pre: we know Adrien will pass a string from a file or pasted by the user.
 	 * 
-	 * API design: -------------------------------------------------------------- 
-	 * 1.get parsed notes and other information in the Parser class. 
+	 * API design: 
+	 * -------------------------------------------------------------- 
+	 * 1. get parsed notes and other information in the Parser class. 
 	 * 2. pass the data from step 1 to another process and convert to XML using XML DOM. 
 	 * 		2.1. we need a really good understanding of musicXML format. 
-	 * 3. pass the XML as a file or
-	 * string to the presenter to be displayed or download. 3.1. will have access
+	 * 3. pass the XML as a file or string to the presenter to be displayed or download. 3.1. will have access
 	 * points for this.
 	 *
 	 */
@@ -30,8 +30,12 @@ public class Parser {
 		tokens = lexer.tokenize();
 	}
 
-	// parse method for parsing tokens:
 	public String parse() {
+		/*
+		 * prerequisites: we need to understand how a note is formated in musicXML our
+		 * design could change based on the way notes are represented in XML.
+		 */
+
 		if (tokens == null || tokens.size() == 0)
 			return "";
 
@@ -39,11 +43,6 @@ public class Parser {
 
 		// contains note and token objects
 		ArrayList<ArrayList<Object>> data = new ArrayList<>();
-
-		/*
-		 * prerequisites: we need to understand how a note is formated in musicXML our
-		 * design could change based on the way notes are represented in XML.
-		 */
 
 		for (ArrayList<Token> line : tokens) {
 			String tune = line.get(0).getData();
