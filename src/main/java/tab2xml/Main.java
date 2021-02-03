36e221f165c903d810ef0733d756f9e388dc5c31
Adrien Hopkins <ahopk127@my.yorku.ca>
2021-02-03 12:03:01 -0500

    merge conflict resolve

--- src/main/java/tab2xml/Main.java
+++ src/main/java/tab2xml/Main.java
@@ -8,12 +8,7 @@ public class Main {
 	 * Executes the program using the Single Entry View.
 	 */
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		View.createView(View.ViewType.SINGLE_ENTRY);
-=======
-		System.out.println("test commit");
-		System.out.println("sayed commit");
->>>>>>> refs/remotes/origin/feature_edward
 	}
 	
 }
