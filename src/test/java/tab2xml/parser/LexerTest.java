10b2c7ddfa272438679de86cf632156e0425c1ce
Sayed Mohammed <sayed08@my.yorku.ca>
2021-02-09 21:23:06 -0500

    made LexerTest

---
+++ src/test/java/tab2xml/parser/LexerTest.java
@@ -0,0 +1,29 @@
+package tab2xml.parser;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LexerTest {
+
+	@Test
+	void testLexer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testTokenize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testTokenizeGuitar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testTokenizeDrum() {
+		fail("Not yet implemented");
+	}
+
+}
