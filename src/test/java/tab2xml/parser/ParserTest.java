1f9c9ed3deeb8478be97aec336493a34e0d521b0
amir <amirabdalla23@gmail.com>
2021-02-16 22:18:17 -0500

    Fixed parser tests.

--- src/test/java/tab2xml/parser/ParserTest.java
+++ src/test/java/tab2xml/parser/ParserTest.java
@@ -10,29 +10,22 @@ class ParserTest {
 	
 	@Test
 	void testTokenizeGuitar() {
-		String sample = "|-----------0-----|-0---------------|\n" + "|---------0---0---|-0---------------|\n"
+		String sample = "|-----------0-----|-0--------rrrrrrrrrrrrrr-------|\n" + "|---------0---0---|-0---------------|\n"
 				+ "|-------1-------1-|-1---------------|\n" + "|-----2-----------|-2---------------|\n"
-				+ "|---2-------------|-2---------------|\n" + "|-0---------------|-0---------------|";
+				+ "|---2-------rrrrrrrrrrrrrrr------|-2---------------|\n" + "|-0--------rrrrrrrrrrrrrrrrrrr-------|-0---------------|\n";
 		
-		String expected = "| 0 | 0 | \n" + "| 0 0 | 0 | \n" +  "| 1 1 | 1 | \n" + "| 2 | 2 | \n" + "| 2 | 2 | \n" + "| 0 | 0 | \n";
+		String expected = "|-----------0-----|-0---------------|\n" + "|---------0---0---|-0---------------|\n"
+				+ "|-------1-------1-|-1---------------|\n" + "|-----2-----------|-2---------------|\n"
+				+ "|---2-------------|-2---------------|\n" + "|-0---------------|-0---------------|\n";
 		
 		Lexer lx2 = new Lexer(sample, Instrument.GUITAR);
 		ArrayList<ArrayList<Token>> tokens = lx2.tokenize();
-		
-		int expectedCount = 32;
-		int count = 0;
-		for (ArrayList<Token> line: tokens) {
-			for (@SuppressWarnings("unused") var t: line)
-				count++;
-		}
-		assertTrue(count == expectedCount);
-		
+
 		StringBuilder sb = new StringBuilder();
 		
 		for (ArrayList<Token> line: tokens) {
 			for (Token t: line) {
 				sb.append(t.getData());
-				sb.append(" ");
 			}
 			sb.append("\n");
 		}
@@ -54,8 +47,8 @@ class ParserTest {
 		
 		Parser parser = new Parser(sample, Instrument.GUITAR);
 		InvalidInputException thrown = assertThrows(InvalidInputException.class, () -> parser.parse(),
-				"The string does not have a proper tune.");
-		assertTrue(thrown.getMessage().contains("The string does not have a proper tune."));
+				"String 1 does not have a proper tune.");
+		assertTrue(thrown.getMessage().contains("String 1 does not have a proper tune."));
 	}
 	
 
