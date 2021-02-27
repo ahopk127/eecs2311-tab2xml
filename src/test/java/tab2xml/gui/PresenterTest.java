9fd66be18ee04c4d9602ef629fe901fa90caffc9
Adrien Hopkins <ahopk127@my.yorku.ca>
2021-02-27 17:38:30 -0500

    Added a new test (testConvertAndSave) to the Presenter

--- src/test/java/tab2xml/gui/PresenterTest.java
+++ src/test/java/tab2xml/gui/PresenterTest.java
@@ -4,10 +4,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import tab2xml.parser.Instrument;
 import tab2xml.parser.Parser;
@@ -59,6 +62,62 @@ class PresenterTest {
 		view.setSelectedInstrument(instrument);
 		presenter.convert();
 		assertEquals(expectedOutput, view.getOutputText());
+	}
+	
+	/**
+	 * Tests that the "Convert and Save" functionality works properly. Like
+	 * {@link #testConvert}, it does not test that the outputted MusicXML is
+	 * correct, only that the Presenter correctly uses the backend code and
+	 * correctly saves the converted MusicXML.
+	 *
+	 * @param showInView argument passed to {@link Presenter#convertAndSave}. The
+	 *                   functionality of this argument is also tested.
+	 * @since 2021-02-27
+	 */
+	@ParameterizedTest
+	@ValueSource(booleans = { true, false })
+	final void testConvertAndSave(boolean showInView) {
+		// SETUP - get input and expected output text
+		final String input, expectedOutput;
+		final Instrument instrument = Instrument.GUITAR;
+		try {
+			final Path TEST_INPUT_FILE = TEST_FILES.resolve("example-e-major.txt");
+			input = Files.readString(TEST_INPUT_FILE);
+			expectedOutput = new Parser(input, instrument).parse();
+		} catch (final Exception e) {
+			e.printStackTrace();
+			assumeTrue(false,
+					e.getClass() + " occured during setup of testLoadFromFile().");
+			return;
+		}
+		
+		// setup view and presenter
+		final ViewBot view = View.createViewBot();
+		final Presenter presenter = new Presenter(view);
+		final Path TEST_FILE = TEST_FILES.resolve("test-write.txt");
+		
+		view.setOutputText("");
+		view.setInputText(input);
+		view.setSelectedInstrument(instrument);
+		view.setSelectedFile(TEST_FILE);
+		
+		presenter.convertAndSave(showInView);
+		
+		// if showInView is true, output should be in output text
+		// if showInView is false, output should NOT be in output text
+		if (showInView) {
+			assertEquals(expectedOutput, view.getOutputText());
+		} else {
+			assertEquals("", view.getOutputText());
+		}
+		
+		// test that saving worked
+		try {
+			assertEquals(expectedOutput, Files.readString(TEST_FILE));
+		} catch (final IOException e) {
+			e.printStackTrace();
+			fail("I/O Error occured during testing: " + e.getMessage());
+		}
 	}
 	
 	/**
