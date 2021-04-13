75a5f8cfb512863800aa7a9c43a010f25e445f5e
Edward Hickman <hickmane@my.yorku.ca>
2021-04-13 18:33:37 -0400

    minor testing documentation changes

--- src/test/java/tab2xml/parser/ParserTest.java
+++ src/test/java/tab2xml/parser/ParserTest.java
@@ -55,7 +55,7 @@ class ParserTest {
 	 * </pre>
 	 */
 	@Test
-	void testConversionGuitar_0() {
+	void testGuitarConversion_0() {
 		final String input;
 		final Instrument instrument = Instrument.GUITAR;
 
